efbb52b4b61ba8d7f20bef5d1fa5d846752fe5da
Stephen Lane <stephen@deveire.com>
2017-04-18 17:42:34 +0100

    Update env-setup.sh so that when a project is not found on psd github, it will use an empty barebones project instead. Additionally, when no ldap user supplied barebones tomcat conf files are used rather than copying them from a qa instance.

--- brightspot/env-setup.sh
+++ brightspot/env-setup.sh
@@ -6,6 +6,7 @@ LDAP_USER="$2"
 MYSQL_USER="root"
 MYSQL_PASS=""
 MYSQL_DB=${PROJECT}
+BASE_PROJECT="0"
 
 TOMCAT_PATH="http://archive.apache.org/dist/tomcat/tomcat-8/v8.0.32/bin/"
 TOMCAT_DIR="apache-tomcat-8.0.32"
@@ -15,8 +16,14 @@ SOLR_PATH="http://archive.apache.org/dist/lucene/solr/4.8.1/"
 SOLR_DIR="solr-4.8.1"
 SOLR_EXT=".tgz"
 
-CODE="https://github.com/perfectsense/${PROJECT}.git"
-
+# use psd project repo if it exists, otherwise use the base project
+git ls-remote "https://github.com/perfectsense/${PROJECT}.git" > /dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+    CODE="https://github.com/stephen-lane/base-project.git"
+    BASE_PROJECT="1"
+else
+	CODE="https://github.com/perfectsense/${PROJECT}.git"
+fi
 
 command_exists () {
 	if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -63,23 +70,37 @@ mv config-5.xml "${TOMCAT_DIR}/solr/collection1/conf/solrconfig.xml"
 mv schema-12.xml "${TOMCAT_DIR}/solr/collection1/conf/schema.xml"
 
 
-echo "Get tomcat configs from qa..."
-scp "${LDAP_USER}@qa.${PROJECT}.psdops.com:/servers/${PROJECT}/conf/server.xml" .
-scp "${LDAP_USER}@qa.${PROJECT}.psdops.com:/servers/${PROJECT}/conf/context.xml" .
+echo "Get tomcat configs from qa, or use base config if no ldap user supplied ..."
+if [ -z "$2" ]; then
+    echo "Using bare bones tomcat config.."
+    wget https://raw.githubusercontent.com/stephen-lane/scripts/master/brightspot/templates/context.xml
+	wget https://raw.githubusercontent.com/stephen-lane/scripts/master/brightspot/templates/server.xml
+else
+	scp "${LDAP_USER}@qa.${PROJECT}.psdops.com:/servers/${PROJECT}/conf/server.xml" .
+	scp "${LDAP_USER}@qa.${PROJECT}.psdops.com:/servers/${PROJECT}/conf/context.xml" .
+fi
+
 
 echo "Setting up tomcat configs to work locally..."
 # server.xml customisation
 sed -i "" 's|jdbc:mysql://.*:3306|jdbc:mysql://localhost:3306|g' server.xml
 sed -i "" 's|${private_ip}||g' server.xml
 sed -i "" 's|:3306/.*?|:3306/'$MYSQL_DB'?|g' server.xml
-sed -i "" 's|username=".*"|username="'$MYSQL_USER'"|g' server.xml
-sed -i "" 's|password=".*"|password="'$MYSQL_PASS'"|g' server.xml
+sed -i "" 's/\([[:blank:]]username="\)[^"]*"/\1'$MYSQL_USER'"/' server.xml
+sed -i "" 's/\([[:blank:]]password="\)[^"]*"/\1'$MYSQL_PASS'"/' server.xml
 # context.xml customisation with sed, bit hacky but its the best that can be done without an xml parser.
 sed -i "" 's|solr/readServerUrl".*"http://.*:8180/solr|solr/readServerUrl" override="false" type="java.lang.String" value="http://localhost:8080/solr|g' context.xml
 sed -i "" 's|solr/serverUrl".*"http://.*:8180/solr|solr/serverUrl" override="false" type="java.lang.String" value="http://localhost:8080/solr|g' context.xml
 sed -i "" 's|dari/recalculationTaskHost".*internal"|dari/recalculationTaskHost" value="0.0.0.0"|g' context.xml
-# append solr home element into to context root element
+
+# add dari/storage/local/rootPath value for bare bones install
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -z "$2" ]; then
+	sed -i "" 's|{{tomcat_dir}}|${CUR_DIR}/${TOMCAT_DIR}|g' context.xml
+	mkdir "${CUR_DIR}/${TOMCAT_DIR}/webapps/storage"
+fi
+
+# append solr home element into to context root element
 SOLR_HOME="${CUR_DIR}/${TOMCAT_DIR}/solr"
 sed -i "" 's|<Context.*>|<Context reloadable="true" allowLinking="true">\
 <Environment name="solr/home" override="false" type="java.lang.String" value="'${SOLR_HOME}'"/>|' context.xml
@@ -106,6 +127,11 @@ else
 fi
 
 git clone "${CODE}"
+# if this is a base project then rename with project name
+if [ $BASE_PROJECT -eq "1" ]; then
+	mv base-project "${PROJECT}"
+	sed -i "" 's|<artifactId>base-project</artifactId>|<artifactId>'${PROJECT}'</artifactId>|g' "${PROJECT}/pom.xml"
+fi
 
 echo "Build project..."
 mvn -f "${PROJECT}/pom.xml" clean install
